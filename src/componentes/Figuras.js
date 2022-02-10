d3788930849d5f77207d10c30c5558ad19de843e
kevinGib <ksd18@hotmail.es>
2022-02-09 22:04:21 -0600

    color verde

--- src/componentes/Figuras.js
+++ src/componentes/Figuras.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Figuras = () => {
   return (
-    <h1> segundo proyecto en react</h1>
+    <h1 className='figuras-h1'> segundo proyecto en react</h1>
   )
 }
